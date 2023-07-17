a361c847c680e02382d17061b78778677729aff8
Tok-Amak <stev-pat@hotmail.fr>
2023-07-17 09:16:00 +0200

    Optimisation

--- assets/script.js
+++ assets/script.js
@@ -17,33 +17,51 @@ const slides = [
 	}
 ]
 //compteur des Slides // numero des slides
+
 let slideDefaut = 0
+const bannerImg = document.querySelector(".banner-img")
+const textTest = document.querySelector(".banner-text")
+
+// boucle pour ajouter les dot en fonction du nombre de slides
+
+for (let compteurBalise = 0; compteurBalise < slides.length; compteurBalise++){
+	//crée un element <p></p>
+	const baliseDot = document.createElement("p")
+	//ajoute la classe dot dans la balise
+	baliseDot.classList.add("dot")
+	//selection du parent ou inserer notre balise <p></p>
+	const baliseDots = document.querySelector(".dots")
+	//mise en place de la liaison parent / enfant
+	baliseDots.appendChild(baliseDot)
+	//creation et mise en place du dot de depart en selected
+	
+}
+const baliseDotAll = document.querySelectorAll(".dot")
+baliseDotAll[0].classList.add("dot_selected")
+
+
 
 //fleche et fonctionement ===== >
 //selection de la fleche et mise en place de la fonction
-let baliseArrowLeft = document.querySelector(".arrow_left")
+const baliseArrowLeft = document.querySelector(".arrow_left")
 baliseArrowLeft.addEventListener("click", function() {
 	//cela effectue un compte pour gerer les slide en cours et le compteur lorsque'on apuie sur les fleche
 	slideDefaut--
 	if (slideDefaut < 0) {
 		slideDefaut = 3
 	}
-	let slideEnCours = slides[slideDefaut]
+	//let slideEnCours = slides[slideDefaut]
 	//fin de l'etape
 	//changement de attribue / image avec pour base le compte precedent
-	let bannerImg = document.querySelector(".banner-img")
 	bannerImg.setAttribute("src", "./assets/images/slideshow/"+slides[slideDefaut].image)
 	//fin de l'etape
 	//mise en place du innerhtml pour inserer le text mis en place dans le tableau "slides"
-	let textTest = document.querySelector(".banner-text")
 	textTest.innerHTML = slides[slideDefaut].tagLine
 	//fin de l'etape
-	let BaliseDotAll = document.querySelectorAll(".dot")
-	BaliseDotAll[0].classList.remove("dot_selected")
-	BaliseDotAll[1].classList.remove("dot_selected")
-	BaliseDotAll[2].classList.remove("dot_selected")
-	BaliseDotAll[3].classList.remove("dot_selected")
-	BaliseDotAll[slideDefaut].classList.add("dot_selected")
+	baliseDotAll.forEach((dot) => {
+		dot.classList.remove("dot_selected")
+	})
+	baliseDotAll[slideDefaut].classList.add("dot_selected")
 	
 })
 //fleche et fonctionement ===== >
@@ -55,37 +73,17 @@ baliseArrowRight.addEventListener("click", function() {
 	if (slideDefaut > 3) {
 		slideDefaut = 0
 	}
-	let slideEnCours = slides[slideDefaut]
 	//fin de l'etape
 	//changement de attribue / image avec pour base le compte precedent
-	let bannerImg = document.querySelector(".banner-img")
 	bannerImg.setAttribute("src", "./assets/images/slideshow/"+slides[slideDefaut].image)
 	//fin de l'etape
 	//mise en place du innerhtml pour inserer le text mis en place dans le tableau "slides"
-	let textTest = document.querySelector(".banner-text")
 	textTest.innerHTML = slides[slideDefaut].tagLine
 	//fin de l'etape
-	let BaliseDotAll = document.querySelectorAll(".dot")
-	BaliseDotAll[0].classList.remove("dot_selected")
-	BaliseDotAll[1].classList.remove("dot_selected")
-	BaliseDotAll[2].classList.remove("dot_selected")
-	BaliseDotAll[3].classList.remove("dot_selected")
-	BaliseDotAll[slideDefaut].classList.add("dot_selected")
+	baliseDotAll.forEach((dot) => {
+		dot.classList.remove("dot_selected")
+	})
+	baliseDotAll[slideDefaut].classList.add("dot_selected")
 
 })
 
-// boucle pour ajouter les dot en fonction du nombre de slides
-
-for (let compteurBalise = 0; compteurBalise < slides.length; compteurBalise++){
-	//crée un element <p></p>
-	let baliseDot = document.createElement("p")
-	//ajoute la classe dot dans la balise
-	baliseDot.classList.add("dot")
-	//selection du parent ou inserer notre balise <p></p>
-	let baliseDots = document.querySelector(".dots")
-	//mise en place de la liaison parent / enfant
-	baliseDots.appendChild(baliseDot)
-	//creation et mise en place du dot de depart en selected
-	let BaliseDotAll = document.querySelectorAll(".dot")
-	BaliseDotAll[0].classList.add("dot_selected")
-}
